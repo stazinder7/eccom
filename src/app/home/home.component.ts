f264a7ae100097da09ac05104bc67189c09efec2
stajinder7 <tajinder.singh+7@venturepact.com>
2023-09-20 09:07:16 +0530

    fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,36 +19,36 @@ export class HomeComponent implements OnInit {
   }
 
   ngAfterViewInit() {
-    jQuery('.carouselArrow__left').click(function () {
-      jQuery('.latestProduct').trigger('prev.owl.carousel');
-    }),
-    jQuery('.carouselArrow__next').click(function () {
-      jQuery('.latestProduct').trigger('next.owl.carousel');
-    }),
-    jQuery('.BestSellercarouselArrow__left').click(function () {
-      jQuery('.BestSellerProduct').trigger('prev.owl.carousel');
-    }),
-    jQuery('.BestSellercarouselArrow__next').click(function () {
-      jQuery('.BestSellerProduct').trigger('next.owl.carousel');
-    }),
-    jQuery('.latestProduct,.BestSellerProduct').owlCarousel({
+    // jQuery('.carouselArrow__left').click(function () {
+    //   jQuery('.latestProduct').trigger('prev.owl.carousel');
+    // }),
+    // jQuery('.carouselArrow__next').click(function () {
+    //   jQuery('.latestProduct').trigger('next.owl.carousel');
+    // }),
+    // jQuery('.BestSellercarouselArrow__left').click(function () {
+    //   jQuery('.BestSellerProduct').trigger('prev.owl.carousel');
+    // }),
+    // jQuery('.BestSellercarouselArrow__next').click(function () {
+    //   jQuery('.BestSellerProduct').trigger('next.owl.carousel');
+    // }),
+    // jQuery('.latestProduct,.BestSellerProduct').owlCarousel({
 
-      loop: false,
-      autoplay: false,
-      margin: 30,
-      nav: false,
-      dots: false,
-      responsive: {
-        0: {
-          items: 1
-        },
-        600: {
-          items: 3
-        },
-        1000: {
-          items: 4
-        }
-      }
-    })
+    //   loop: false,
+    //   autoplay: false,
+    //   margin: 30,
+    //   nav: false,
+    //   dots: false,
+    //   responsive: {
+    //     0: {
+    //       items: 1
+    //     },
+    //     600: {
+    //       items: 3
+    //     },
+    //     1000: {
+    //       items: 4
+    //     }
+    //   }
+    // })
   }
 }
